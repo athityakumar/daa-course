05812365480176c4bc9eabd5a586c044f3a5996d
athityakumar <athityakumar@gmail.com>
2016-07-30 18:28:27 +0530

    Added pdf/ folder checking & file download status

--- downloader.rb
+++ downloader.rb
@@ -4,6 +4,13 @@ mechanize.follow_meta_refresh = true
 mechanize.verify_mode = OpenSSL::SSL::VERIFY_NONE
 mechanize.pluggable_parser.default = Mechanize::Download 
 links = ["BFS","DFS"]
+unless Dir.exist? "pdf"
+  Dir.mkdir("pdf") 
+end
+Dir.chdir("pdf")
+existing_files = Dir.entries(".")
+existing_files.delete(".")
+existing_files.delete("..")
 for i in (1..22)
   if i < 10
     links.push("Lecture-0#{i.to_s}")
@@ -11,8 +18,11 @@ for i in (1..22)
     links.push("Lecture-#{i.to_s}")
   end
 end
-Dir.chdir("pdf")
 links.each do |link|
-  mechanize.get("http://www.facweb.iitkgp.ernet.in/~sourav/#{link}.pdf").save
-  puts "Downloaded #{link}.pdf"
+  if existing_files.include? link+".pdf"
+    puts "#{link}.pdf already exists"
+  else
+    mechanize.get("http://www.facweb.iitkgp.ernet.in/~sourav/#{link}.pdf").save
+    puts "Downloaded #{link}.pdf"
+  end
 end
